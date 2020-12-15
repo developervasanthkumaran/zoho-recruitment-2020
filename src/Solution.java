ec6d5f3ab4e2c7f4d258df5e769bdbc9ed780d0a
vasanthtk <developer.vasanthkumaran@gmail.com>
2020-12-15 10:13:03 +0530

    solution

--- src/Solution.java
+++ src/Solution.java
@@ -4,8 +4,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 class Solution {
+
+    void printPattern(int n){
+        n+=1;
+        int k = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                System.out.print(' ');
+            }
+            int x = i;
+            while (x > 0){
+                System.out.print((x--));
+            }
+            int y = 0;
+            while (y <= i){
+                System.out.print((y++));
+            }
+            for (int j = 0; j < k; j++) {
+                System.out.print(' ');
+            }
+            System.out.println();
+            k--;
+        }
+    }
+
+
     public static void main(String[] args) {
             FastReader reader = new FastReader();
+            Solution solution = new Solution();
+            int lines = reader.nextInt();
+            solution.printPattern(lines);
     }
 }
 
